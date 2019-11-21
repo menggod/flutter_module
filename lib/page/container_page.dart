493a8aca40f3a53e56fad9bda088432ecd22ce39
menggod <menggod@126.com>
2019-11-21 17:00:19 +0800

    backup

--- lib/page/container_page.dart
+++ lib/page/container_page.dart
@@ -5,13 +5,12 @@ import 'package:flutter_module/widget/tab_box.dart';
 void main() => runApp(ParentWidget());
 
 class ContainerPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Container(
       child: Center(
-        child: TapBoxA(),
+        child: Container(child: TapBoxA()),
       ),
       decoration: BoxDecoration(color: Colors.white),
     ));
